44e98deeae801c27058679ba59ff09af9aa07a7f
OlaIfemi667 <bancolejeandedieu@gmail.com>
2024-08-21 08:31:15 +0100

    fix: models.py and views.py in journey_app

--- journey_app/booking/urls.py
+++ journey_app/booking/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "journey_app"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("booking_page/", views.booking_page, name="booking_page"),
 ]
--- journey_app/booking/views.py
+++ journey_app/booking/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {
+        'welcome_message': "Welcome to the Polls App!",
+        'vehicles': LES_TYPES_DE_VEHICULES
+    }
+    return render(request, 'index.html', context)
 
-def booking_page(client):
-    return HttpResponse("Hi, " % question_id % ", you can book here  ")
+def booking_page(request):
+    if request.method == "POST":
+        # Traitez les données de la réservation ici, par exemple, en créant un nouvel objet Booking
+        booking_with_escale = request.POST.get('booking_with_escale')
+        places_reservees = request.POST.get('places_reservees')
+        booked_vehicles = request.POST.get('booked_vehicles')
+
+        # Pour cet exemple, on renvoie simplement les données, mais vous voudrez probablement les sauvegarder dans la base de données
+        return HttpResponse(f"Réservation créée avec succès : {places_reservees} places réservées en {LES_TYPES_DE_VEHICULES[booked_vehicles]} avec escale : {booking_with_escale}")
+    return render(request, 'booking_page.html', {"LES_TYPES_DE_VEHICULES": LES_TYPES_DE_VEHICULES})
